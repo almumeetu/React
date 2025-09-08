4f155926cd43dadbc1747f9114de622d427207c6
Al-Mumeetu Saikat <113462809+almumeetu@users.noreply.github.com>
2025-09-08 15:31:45 +0600

    update

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,11 +1,12 @@
 
-function App() {
-
+const App = () => {
   return (
-    <>
-      <h1>Hello Saikat !</h1>
-    </>
-  )
-}
+    <div>
+        <h1>Hello Saikat!</h1>
+        <input type="mail" placeholder="Enter Your Mail" />
+        <button>Submit</button>
+    </div>
+  );
+};
 
-export default App
+export default App;
