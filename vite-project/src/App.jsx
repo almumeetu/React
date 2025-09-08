fb2ba709255745f1a93608f30dc26ef1805f1516
Al-Mumeetu Saikat <113462809+almumeetu@users.noreply.github.com>
2025-09-08 16:13:35 +0600

    make 4 components

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -2,9 +2,7 @@
 const App = () => {
   return (
     <div>
-        <h1>Hello Saikat!</h1>
-        <input type="mail" placeholder="Enter Your Mail" />
-        <button>Submit</button>
+      
     </div>
   );
 };
