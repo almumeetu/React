13253dcc2044786de9bb4db4d4b41b20f15dcdea
Al-Mumeetu Saikat <113462809+almumeetu@users.noreply.github.com>
2025-09-08 18:06:28 +0600

    working sort hand if, else

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,25 +1,14 @@
-import ContactForm from "./assets/component/ContactForm";
-import Header from "./assets/component/Header";
-import Hero from "./assets/component/Hero";
-import Footer from './assets/component/Footer';
+
 
 const App = () => {
+
+  let age = "21";
   return (
     <div>
-       <Header />
-       <Hero />
-       <ContactForm />
-       <Footer />
+       {age > 20 ? <h1>You are eligible to vote</h1> : <h1>You are not eligible to vote</h1>}
     </div>
   );
 };
 
 export default App;
 
-//JSX CONVENTIONS
-//1. You Need to return a Single parent Element in JSX
-//2. You can Implement JS Directly in JSX by using {}
-//3. All tags Self-close in JSX
-//4. ClassName and HTMLFor, not class and for in JSX
-//5. Write all HTML Attributes in camelCase in JSX
-//6. Write Inline Styles as Object in JSX
